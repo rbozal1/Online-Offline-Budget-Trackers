07b12da1c31d1820db96a2cf0c7eaf1e2493fbf3
Roger Steve <roxhersbozali@gmail.com>
2021-01-06 21:52:02 -0500

    deploying

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/budget", 
